0f54c9e45ba64c6f0a43e5f9edcbc016f84e6a88
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2018-03-30 15:14:00 +0300

    move keys_manager to shared_model
    Signed-off-by: Alexey Chernyshov <alexey.n.chernyshov@gmail.com>

--- iroha-cli/main.cpp
+++ iroha-cli/main.cpp
@@ -32,10 +32,9 @@
 #include "validators.hpp"
 
 // Account information
-DEFINE_bool(
-    new_account,
-    false,
-    "Generate and save locally new public/private keys");
+DEFINE_bool(new_account,
+            false,
+            "Generate and save locally new public/private keys");
 DEFINE_string(account_name,
               "",
               "Name of the account. Must be unique in iroha network");
@@ -61,7 +60,6 @@ DEFINE_string(peers_address,
 
 // Run iroha-cli in interactive mode
 DEFINE_bool(interactive, true, "Run iroha-cli in interactive mode");
-
 
 using namespace iroha::protocol;
 using namespace iroha::model::generators;
@@ -158,20 +156,17 @@ int main(int argc, char *argv[]) {
       return EXIT_FAILURE;
     }
     iroha::KeysManagerImpl manager((path / FLAGS_account_name).string());
-    boost::optional<iroha::keypair_t> keypair;
-    if (FLAGS_pass_phrase.size() != 0) {
-      keypair = manager.loadKeys(FLAGS_pass_phrase);
-    } else {
-      keypair = manager.loadKeys();
-    }
+    auto keypair = FLAGS_pass_phrase.size() != 0
+        ? manager.loadKeys(FLAGS_pass_phrase)
+        : manager.loadKeys();
     if (not keypair) {
       logger->error(
           "Cannot load specified keypair, or keypair is invalid. Path: {}, "
-          "keypair name: {}. Use --key_path to path to your keypair. \nMaybe wrong pass phrase (\"{}\")?",
+          "keypair name: {}. Use --key_path to path to your keypair. \nMaybe "
+          "wrong pass phrase (\"{}\")?",
           path.string(),
           FLAGS_account_name,
-          FLAGS_pass_phrase
-      );
+          FLAGS_pass_phrase);
       return EXIT_FAILURE;
     }
     // TODO 13/09/17 grimadas: Init counters from Iroha, or read from disk?
@@ -183,7 +178,7 @@ int main(int argc, char *argv[]) {
         0,
         0,
         std::make_shared<iroha::model::ModelCryptoProviderImpl>(
-            *keypair));
+            *std::unique_ptr<iroha::keypair_t>(keypair->makeOldModel())));
     interactiveCli.run();
   } else {
     logger->error("Invalid flags");
